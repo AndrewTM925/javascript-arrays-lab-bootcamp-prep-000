9a35e3f495e88fcb36849821ffd58084544a7dcb
Andrew T Martin III <atmartin93@live.com>
2018-08-01 01:29:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+  return
+}
+
+function kittens() {
+  var kittens = window.kittens()
+  return
+}
+
+function destructivelyAppendKitten(Ralph) {
+  kittens.push(Ralph)
+  window.kittens
+  return
+}
+
+function destructivelyPrependKitten(Bob) {
+  kittens.unshift(Bob)
+  window.kittens
+  return
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  window.kittens
+  return
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  window.kittens
+  return
+}
+
+function appendKitten(Broom) {
+
+  var kittens = ("Milo", "Otis", "Garfield")
+  kittens.unshift("Broom")
+
+  return window.kittens
+}
